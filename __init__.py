11b48039f4ffa4fe040e889314f4ee845110c6be
Mohamed Eid <78418874+gomgom-40@users.noreply.github.com>
2025-12-22 14:55:01 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,34 +1,17 @@
-import esphome.codegen as cg
+from esphome.codegen import esphome_ns
 import esphome.config_validation as cv
-from esphome.components import uart
-from esphome.const import CONF_ID
+from esphome.const import CONF_ID, CONF_MIN_RANGE, CONF_MAX_RANGE, CONF_SENSITIVITY
 
-DEPENDENCIES = ["uart"]
 
-RD03_NS = cg.esphome_ns.namespace("RD_03_presence_radar")
-RD03Radar = RD03_NS.class_("RD03Radar", cg.Component, uart.UARTDevice)
+RD03_NS = esphome_ns.namespace('RD_03_presence_radar')
+RD03Radar = RD03_NS.class_('RD03Radar', cg.Component)
 
-CONF_MIN_RANGE = "min_range"
-CONF_MAX_RANGE = "max_range"
-CONF_SENSITIVITY = "sensitivity"
-CONF_MAX_ABSENCE = "max_absence"
 
-CONFIG_SCHEMA = uart.UART_DEVICE_SCHEMA.extend(
-    {
-        cv.GenerateID(): cv.declare_id(RD03Radar),
-        cv.Optional(CONF_MIN_RANGE, default=20.0): cv.float_range(min=10, max=300),
-        cv.Optional(CONF_MAX_RANGE, default=500.0): cv.float_range(min=50, max=600),
-        cv.Optional(CONF_SENSITIVITY, default=3): cv.int_range(min=1, max=5),
-        cv.Optional(CONF_MAX_ABSENCE, default=300): cv.positive_int,
-    }
-).extend(cv.COMPONENT_SCHEMA)
-
-async def to_code(config):
-    var = cg.new_Pvariable(config[CONF_ID])
-    await cg.register_component(var, config)
-    await uart.register_uart_device(var, config)
-
-    cg.add(var.set_min_range(config[CONF_MIN_RANGE]))
-    cg.add(var.set_max_range(config[CONF_MAX_RANGE]))
-    cg.add(var.set_sensitivity(config[CONF_SENSITIVITY]))
-    cg.add(var.set_max_absence(config[CONF_MAX_ABSENCE] * 1000))  # minutes to ms
+CONFIG_SCHEMA = cv.All(
+cv.Schema({
+cv.GenerateID(): cv.declare_id(RD03Radar),
+cv.Optional(CONF_MIN_RANGE): cv.float_,
+cv.Optional(CONF_MAX_RANGE): cv.float_,
+cv.Optional(CONF_SENSITIVITY): cv.int_,
+})
+)
