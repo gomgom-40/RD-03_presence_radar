f5a3ee27224e75f692252821d866865633e513ae
Mohamed Eid <78418874+gomgom-40@users.noreply.github.com>
2025-12-22 14:54:15 +0200

    Update binary_sensor.py

--- binary_sensor.py
+++ binary_sensor.py
@@ -1,20 +1,15 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.components import binary_sensor
-from esphome.const import CONF_ID
 from . import RD03Radar
 
-CONF_RD03_ID = "rd03_id"
 
-CONFIG_SCHEMA = binary_sensor.binary_sensor_schema(
-    device_class="occupancy"
-).extend(
-    {
-        cv.GenerateID(CONF_RD03_ID): cv.use_id(RD03Radar),
-    }
-)
+CONFIG_SCHEMA = binary_sensor.binary_sensor_schema().extend({
+cv.GenerateID(): cv.use_id(RD03Radar),
+})
+
 
 async def to_code(config):
-    parent = await cg.get_variable(config[CONF_RD03_ID])
-    sens = await binary_sensor.new_binary_sensor(config)
-    cg.add(parent.set_presence(sens))
+var = await cg.get_variable(config[cv.GenerateID()])
+sens = await binary_sensor.new_binary_sensor(config)
+cg.add(var.set_presence(sens))
