8981d0ef88b08926c16cf085adf21a3e40ee2295
Mohamed Eid <78418874+gomgom-40@users.noreply.github.com>
2025-12-22 14:53:48 +0200

    Update sensor.py

--- sensor.py
+++ sensor.py
@@ -1,21 +1,15 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.components import sensor
-from esphome.const import CONF_ID
 from . import RD03Radar
 
-CONF_RD03_ID = "rd03_id"
 
-CONFIG_SCHEMA = sensor.sensor_schema(
-    unit_of_measurement="cm",
-    accuracy_decimals=0,
-).extend(
-    {
-        cv.GenerateID(CONF_RD03_ID): cv.use_id(RD03Radar),
-    }
-)
+CONFIG_SCHEMA = sensor.sensor_schema(unit_of_measurement="cm").extend({
+cv.GenerateID(): cv.use_id(RD03Radar),
+})
+
 
 async def to_code(config):
-    parent = await cg.get_variable(config[CONF_RD03_ID])
-    sens = await sensor.new_sensor(config)
-    cg.add(parent.set_distance(sens))
+var = await cg.get_variable(config[cv.GenerateID()])
+sens = await sensor.new_sensor(config)
+cg.add(var.set_distance(sens))
